5cb33ad3ee35023ee07ebfc07ac409155f4e3841
Lucas Grohe <lucasgrohegomes@gmail.com>
2025-09-30 19:36:14 -0300

    Ajustes nos diretórios do static

---
+++ static/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './html/**/*.html', // All HTML files inside html folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
